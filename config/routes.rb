573a33495a893ec6d9d54d348dd2a0bc1c78911d
Zach Weber <weberzt@gmail.com>
2020-02-07 11:11:53 -0600

    My Solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get '/restaurants', to: 'restaurants#index'
-  get '/pizzas', to: 'pizzas#index'
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/pizzas', to: 'pizzas#index'
+  resources :pizzas, except: [:edit, :update, :destroy]
+  resources :restaurants, except: [:edit, :update, :destroy]
+  resources :pizza_restaurants, except: [:edit, :update, :destroy]
 end
