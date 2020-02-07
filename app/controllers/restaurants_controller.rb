213374698f1077d54e7dfc1265f8140d361f383d
learn <github email address>
2020-02-07 09:08:34 -0800

    Ran out of time, Time spent looking for typo

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,15 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+    
+  end
+
+  
+
 end
