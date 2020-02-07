0590698c58b0a1cb3715aaf25355ae9825c4f2b7
github username <github email address>
2020-02-07 11:09:43 -0600

    christines solution

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,37 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show 
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+  end
+
+  def create
+    @pizza = Pizza.new(pizza_params)
+    # @restaurant = Restaurant.find(params[:id])
+    # @restaurant_pizza = RestaurantPizza.new(restaurant_id: @restaurant.id, pizza_id: @pizza.id)
+    #TRYING TO CONNECT THEM
+    if @pizza.valid?
+      @pizza.save
+      # @restaurant_pizza.save
+      redirect_to pizza_path(@pizza)
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients)
+  end
+
+
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,8 @@
 class Pizza < ApplicationRecord
+    validates :name, presence: true
+    validates :ingredients, presence: true
+    validates :name, uniqueness: true
+
+    has_many :restaurant_pizzas
+    has_many :restaurants, through: :restaurant_pizzas
 end
