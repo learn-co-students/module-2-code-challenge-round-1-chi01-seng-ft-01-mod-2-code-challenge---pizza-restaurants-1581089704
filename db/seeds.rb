7ddaec1eb0aca56bff5469bf7220e7a7b43d6120
github username <github email address>
2020-02-07 11:07:57 -0600

    my code challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,68 +7,75 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 #   1
-Restaurant.destroy_all
+# Restaurant.destroy_all
  
-restaurants = [{
-  name: "Sottocasa NYC",
-  address: "298 Atlantic Ave, Brooklyn, NY 11201",
-},
-{
-  name: "PizzArte",
-  address: "69 W 55th St, New York, NY 10019",
-},
-{
-  name: "San Matteo NYC",
-  address: "1559 2nd Ave, New York, NY 10028"
-}]
+# restaurants = [{
+#   name: "Sottocasa NYC",
+#   address: "298 Atlantic Ave, Brooklyn, NY 11201",
+# },
+# {
+#   name: "PizzArte",
+#   address: "69 W 55th St, New York, NY 10019",
+# },
+# {
+#   name: "San Matteo NYC",
+#   address: "1559 2nd Ave, New York, NY 10028"
+# }]
 
-restaurants.each do |r|
-  Restaurant.create!(r)
-end
+# restaurants.each do |r|
+#   Restaurant.create!(r)
+# end
 
-pizzas = [
-  {
-    name: "Cheese",
-    ingredients: "Dough, Tomato Sauce, Cheese"
-  },
-  {
-    name: "Pepperoni",
-    ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni"
-  },
-  {
-    name: "California",
-    ingredients: "Dough, Sauce, Ricotta, Red peppers, Mustard"
-  },
-  {
-    name: "Greek",
-    ingredients: "Dough, Sauce, Cheese, Feta, Artichokes, Kalamata Olives"
-  },
-  {
-    name: "Margherita",
-    ingredients: "Dough, Sauce, Cheese, Basil"
-  },
-  {
-    name: "BBQ Chicken",
-    ingredients: "Dough, BBQ sauce, Chicken, Cilantro, Red Onions, Fontina Cheese"
-  },
-  {
-    name: "Hawaiian",
-    ingredients: "Dough, Sauce, Cheese, Ham, Pineapple "
-  },
-  {
-    name: "Meat Lovers",
-    ingredients: "Dough, Sauce, Cheese, Pepperoni, Sausage, Meatballs, Mushrooms"
-  },
-  {
-    name: "Frutti di mare",
-    ingredients: "Dough, Sauce, Cheese, Tuna, Shrimp, Mussels"
-  },
-  {
-    name: "Veggie",
-    ingredients: "Dough, Sauce, Cheese, Red Peppers, Green Peppers, Onions, Mushrooms"
-  }
-]
+# pizzas = [
+#   {
+#     name: "Cheese",
+#     ingredients: "Dough, Tomato Sauce, Cheese"
+#   },
+#   {
+#     name: "Pepperoni",
+#     ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni"
+#   },
+#   {
+#     name: "California",
+#     ingredients: "Dough, Sauce, Ricotta, Red peppers, Mustard"
+#   },
+#   {
+#     name: "Greek",
+#     ingredients: "Dough, Sauce, Cheese, Feta, Artichokes, Kalamata Olives"
+#   },
+#   {
+#     name: "Margherita",
+#     ingredients: "Dough, Sauce, Cheese, Basil"
+#   },
+#   {
+#     name: "BBQ Chicken",
+#     ingredients: "Dough, BBQ sauce, Chicken, Cilantro, Red Onions, Fontina Cheese"
+#   },
+#   {
+#     name: "Hawaiian",
+#     ingredients: "Dough, Sauce, Cheese, Ham, Pineapple "
+#   },
+#   {
+#     name: "Meat Lovers",
+#     ingredients: "Dough, Sauce, Cheese, Pepperoni, Sausage, Meatballs, Mushrooms"
+#   },
+#   {
+#     name: "Frutti di mare",
+#     ingredients: "Dough, Sauce, Cheese, Tuna, Shrimp, Mussels"
+#   },
+#   {
+#     name: "Veggie",
+#     ingredients: "Dough, Sauce, Cheese, Red Peppers, Green Peppers, Onions, Mushrooms"
+#   }
+# ]
 
-pizzas.each do |p|
-  Pizza.create!(p)
-end
+# pizzas.each do |p|
+#   Pizza.create!(p)
+# end
+
+# RestaurantPizza.create(restaurant_id: 1, pizza_id: 1)
+RestaurantPizza.create(restaurant_id: 1, pizza_id: 2)
+RestaurantPizza.create(restaurant_id: 1, pizza_id: 3)
+RestaurantPizza.create(restaurant_id: 1, pizza_id: 4)
+RestaurantPizza.create(restaurant_id: 2, pizza_id: 1)
+RestaurantPizza.create(restaurant_id: 3, pizza_id: 1)
