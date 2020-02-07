eea8ef74d8348714fce39d2b09eb4ce208bdb4cf
github username <github email address>
2020-02-07 11:08:31 -0600

    my solution

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,14 @@
 class Restaurant < ApplicationRecord
-  
+  has_many :restaurant_pizzas
+  has_many :pizzas, through: :restaurant_pizzas
+
+  def pizzas
+    my_restaurant_pizzas = RestaurantPizza.all.select do |rp|
+      rp.restaurant_id == self.id
+    end
+    pizzas = my_restaurant_pizzas.each do |mrp|
+      mrp.pizza_id
+    end
+  end
+
 end
